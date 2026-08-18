3f2ee795dee39efbce671e5fd4f98276f269ebcf
billal7 <76392253+billal7@users.noreply.github.com>
2026-08-18 07:32:19 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // change to '@vitejs/react-swc' if your template uses it
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/bangladesh-web-scope-research/', // Crucial: This points to your GitHub repository subfolder
-})
